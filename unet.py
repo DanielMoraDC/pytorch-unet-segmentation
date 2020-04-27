ecc6b102e13b863ba8be5b896438146ae6c586a8
Daniel <daniel.mora.checa@gmail.com>
2020-04-27 17:56:29 +0200

    Add BN normalization layers
    Proven to ease training during eaely trials.

--- unet.py
+++ unet.py
@@ -93,12 +93,14 @@ class DoubleConv2d(nn.Module):
                       kernel_size=3,
                       stride=1,
                       padding=0),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=out_channels,
                       out_channels=out_channels,
                       kernel_size=3,
                       stride=1,
                       padding=0),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True)
         )
 
