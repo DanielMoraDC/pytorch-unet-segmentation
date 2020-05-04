6259a5576d6aa7b91b3d8bfcd377ca7adc13425b
Daniel <daniel.mora.checa@gmail.com>
2020-05-04 17:47:45 +0200

    Allow initializing network from checkpoint
    Changed some of the default parameters as well.

--- training.py
+++ training.py
@@ -24,6 +24,7 @@ cuda = True if torch.cuda.is_available() else False
 # Parameters
 # ---------------
 
+load_from = 'unet_iter_300000.pt'
 n_epochs = 250
 batch_size = 1
 n_channels = 3
@@ -31,22 +32,37 @@ n_classes = 6 + 1  # background
 data_load_workers = 2
 lr = 0.001
 tensorboard_dir = 'logs'
-stats_interval = 10
+stats_interval = 2500
+save_model_interval = 50000
+image_crop = 512
+image_resize = 575
 
 
 # ---------------
 # Model
 # ---------------
 
-cross_entropy_loss = torch.nn.CrossEntropyLoss()
-unet = Unet(n_channels=n_channels, n_classes=n_classes)
+
+
+if load_from is not None:
+    filename = os.path.basename(load_from)
+    iteration_offset = int(os.path.splitext(filename)[0].split('_')[-1])
+    print(f'Loading weights from checkpoint: {load_from}. '
+          f'Iteration: {iteration_offset}')
+    unet = torch.load(load_from)
+else:
+    print('Initializing model from scratch')
+    unet = Unet(n_channels=n_channels, n_classes=n_classes)
+    unet.apply(initialize_weights)
+    iteration_offset = 0
+
+
 optimizer = torch.optim.SGD(unet.parameters(), lr=lr, momentum=0.99)
+cross_entropy_loss = torch.nn.CrossEntropyLoss()
 
 if cuda:
     cross_entropy_loss.cuda()
     unet.cuda()
-
-unet.apply(initialize_weights)
 
 
 # ---------------
@@ -54,7 +70,7 @@ unet.apply(initialize_weights)
 # ---------------
 
 data_aug = DataAugmentation(
-    central_crop_size=512,
+    central_crop_size=image_crop,
     h_flipping_chance=0.50,
     brightness_rate=0.10,
     contrast_rate=0.10,
@@ -62,7 +78,7 @@ data_aug = DataAugmentation(
     hue_rate=0.05)
 
 dataset = DeepFashion2Dataset(DATA_PATH,
-                              image_resize=575,
+                              image_resize=image_resize,
                               data_augmentation=data_aug)
 
 dataloader = DataLoader(dataset,
@@ -84,7 +100,7 @@ for epoch in range(n_epochs):
 
     for i, (imgs, masks) in enumerate(dataloader):
 
-        iteration = epoch * batches_per_epoch + i
+        iteration = epoch * batches_per_epoch + i + iteration_offset
 
         # Configure batch data
         batch_imgs = imgs.to(device).float()
@@ -110,6 +126,9 @@ for epoch in range(n_epochs):
                                             dim=1).unsqueeze_(1)
             writer.add_images('Predicted', predicted_images, iteration)
             writer.add_images('Groundtruth', masks.unsqueeze_(1), iteration)
+
+        if iteration % save_model_interval == 0:
+            torch.save(unet, f'unet_iter_{iteration}.pt')
 
         print(
             "[Epoch %d/%d] [Batch %d/%d] Loss: %f, Iou: %f"
