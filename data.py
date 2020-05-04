29d027791f164d28baf5688aa406ae38ccf4db5d
Daniel <daniel.mora.checa@gmail.com>
2020-05-04 19:05:48 +0200

    Allow reading data from specific set
    Sets are: train, test, validation.

--- data.py
+++ data.py
@@ -1,19 +1,12 @@
 import os
+import json
 import random
-from glob import glob
-from typing import (
-    List,
-    Tuple
-)
-from collections.abc import Iterable
 from dataclasses import dataclass
 
 import skimage.io
 import skimage.transform
 
 import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
 
 import torch
 from torch.utils.data import Dataset
@@ -54,25 +47,29 @@ class DeepFashion2Dataset(Dataset):
     def __init__(self,
                  path: str,
                  image_resize: int,
+                 subset: str = 'train',
                  data_augmentation: DataAugmentation = None):
         self._image_resize = image_resize
+
+        valid_subsets = ['train', 'validation', 'test']
+        if subset not in valid_subsets:
+            raise ValueError(f'Subset must be one of {valid_subsets}')
+
         self._data_augmentation = data_augmentation
 
-        # Collect all images
-        images_path = os.path.join(path, 'images')
-        images = glob(os.path.join(images_path, '*'))
+        with open(os.path.join(path, 'data.json'), 'r') as file:
+            data = json.load(file)
 
-        masks_path = os.path.join(path, 'masks')
+        images = [os.path.join(path, row['image_path'])
+                  for row in data if row['set'] == subset]
+        masks = [os.path.join(path, row['mask_path'])
+                 for row in data if row['set'] == subset]
 
-        def _mask_path(img_path: str) -> str:
-            image_id = os.path.splitext(os.path.basename(img_path))[0]
-            return os.path.join(masks_path, image_id + '.png')
-
-        # Find image - mask pairs
+        # Read only existing image + mask pairs
         self._image_mask_pairs = np.array([
-            (img_path, _mask_path(img_path))
-            for img_path in images
-            if os.path.isfile(_mask_path(img_path))
+            (img, mask)
+            for (img, mask) in zip(images, masks)
+            if os.path.isfile(img) and os.path.isfile(mask)
         ])
         print(f'Read {len(images)} images, built ' +
               f'{len(self._image_mask_pairs)} image pairs')
@@ -136,6 +133,8 @@ class PairRandomCrop(object):
         return image, mask
 
 
+import matplotlib.pyplot as plt
+
 if __name__ == '__main__':
 
     augmentation = DataAugmentation(
@@ -147,15 +146,16 @@ if __name__ == '__main__':
         hue_rate=0.05
     )
 
-    data = DeepFashion2Dataset('seg_dataset',
+    data = DeepFashion2Dataset('full_dataset',
                                image_resize=580,
                                data_augmentation=augmentation)
 
-    n_rows = 2
+    n_rows = 5
+    idxs = [random.randint(0, len(data)) for _ in range(n_rows)]
     fig, axs = plt.subplots(n_rows, 2, figsize=(7.0, n_rows*3.0))
 
-    for i in range(n_rows):
-        image, mask = data[i]
+    for i, idx in enumerate(idxs):
+        image, mask = data[idx]
         axs[i][0].imshow(np.transpose(image, (1, 2, 0)))
         axs[i][0].set_title('Sample image #{}'.format(i))
         axs[i][1].imshow(mask)
